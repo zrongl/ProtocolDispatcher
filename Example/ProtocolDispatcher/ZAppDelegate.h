e24fce9efa6bb5eae09d93405d9b0d5de2728b33
zrongl <ronglei0324@163.com>
2019-11-01 09:59:58 +0800

    Initial commit

---
+++ Example/ProtocolDispatcher/ZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZAppDelegate.h
+//  ProtocolDispatcher
+//
+//  Created by ronglei0324@163.com on 11/01/2019.
+//  Copyright (c) 2019 ronglei0324@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/ProtocolDispatcher/ZViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZViewController.h
+//  ProtocolDispatcher
+//
+//  Created by ronglei0324@163.com on 11/01/2019.
+//  Copyright (c) 2019 ronglei0324@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZViewController : UIViewController
+
+@end
